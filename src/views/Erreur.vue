629fc725e5820052ce894ff0352a6f4e2a16117c
isabelle913 <atelierisabelle@videotron.ca>
2023-12-09 14:45:40 -0500

    page erreur

--- src/views/Erreur.vue
+++ src/views/Erreur.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Erreur</div>
+  <div
+    class="container mx-auto flex flex-col justify-center items-center h-screen page-erreur text-center p-6">
+    <div>Oups, il y a une erreur à quelque part!!</div>
+  </div>
 </template>
 
 <script>
@@ -9,3 +12,9 @@ export default {
   inject: ["addTeams", "teams"],
 };
 </script>
+<style scoped>
+.page-erreur {
+  font-family: "Kalnia", serif;
+  font-size: 60px;
+}
+</style>
