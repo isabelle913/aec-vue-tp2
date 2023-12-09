6232512d152ba1ccf987904588f41f28c92baca0
isabelle913 <atelierisabelle@videotron.ca>
2023-12-09 08:22:12 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="underline-offset-2 text-center my-6">Hello World!</div>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>This is an home view</h1>
+  </div>
+</template>
